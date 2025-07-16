56ff8d6e8cc44cba26318fba1361d5732ded5b0e
codebyazeredo <matheus.azeredo@hotmail.com>
2025-07-16 09:44:03 -0300

    feat: logica para cpf

---
+++ cpf/formatarCpf.js
@@ -0,0 +1,6 @@
+function formatarCPF(cpf) {
+  cpf = cpf.replace(/\D/g, '');
+  return cpf.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/, '$1.$2.$3-$4');
+}
+
+module.exports = { formatarCPF };
--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
 const rateLimit = require('express-rate-limit');
+const { validarCPF } = require('./cpf/validarCpf');
+const { formatarCPF } = require('./cpf/formatarCpf');
+const { gerarCPF } = require('./cpf/gerarCpf');
 
 const app = express();
 
