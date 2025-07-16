81dab3d5f02103d7fd96ba5f6016470260a8991e
codebyazeredo <matheus.azeredo@hotmail.com>
2025-07-16 14:58:35 -0300

    feat: melhorada nomenclatura

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const { gerarCNPJ } = require('./cnpj/gerarCnpj');
 const { consultarCNPJ } = require('./cnpj/consultarCnpj');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(helmet());
